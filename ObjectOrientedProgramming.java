3287b4307a3e8d4d0b1d811e0827a5ef8fdccb5d
Kartik Jaiswal <kartikjaiswal038@gmail.com>
2024-02-04 22:27:43 +0530

    ObjectOrientedProgramming

--- ObjectOrientedProgramming.java
+++ ObjectOrientedProgramming.java
@@ -1,6 +1,19 @@
+class Calculator {
+    private int sum;
+
+    public int add(int a, int b) {
+        sum = a + b;
+        return sum;
+    }
+}
+
 public class ObjectOrientedProgramming {
-    
+
     public static void main(String[] args) {
-        
+        // Object: Properties and Behaviour
+
+        // Creating the object
+        Calculator calc = new Calculator();
+        System.out.println("Sum of 5 and 6 is:"+ calc.add(5, 6));
     }
 }
---
+++ rough.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> findDuplicates(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<int> ans;
+
+    for (int i = 0; i < n; i++)
+    {
+        int index = abs((nums[i] % (n + 1)) - 1);
+        nums[index] = nums[index] + n + 1;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int temp = nums[i] / (n + 1);
+        if (temp >= 2)
+            ans.push_back(i + 1);
+    }
+    return ans;
+}
+
+void nextDistinct(int *arr, int size, int &index)
+{
+    while (index < size && arr[index] == arr[index - 1])
+        index++;
+}
+
+int doUnion(int arr[], int n, int brr[], int m)
+{
+    int i = 0;
+    int j = 0;
+    int ans = 0;
+    sort(arr, arr + n);
+    sort(brr, brr + m);
+    while (i < n && j < m)
+    {
+        if (arr[i] < brr[j])
+        {
+            nextDistinct(arr, n, ++i);
+        }
+        else if (arr[i] > brr[j])
+        {
+            nextDistinct(brr, m, ++j);
+        }
+        else
+        {
+            nextDistinct(arr, n, ++i);
+            nextDistinct(brr, m, ++j);
+        }
+        ans++;
+    }
+
+    while (i < n)
+    {
+        ans++;
+        nextDistinct(arr, n, ++i);
+    }
+    while (j < m)
+    {
+        ans++;
+        nextDistinct(brr, m, ++j);
+    }
+
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 1};
+    findDuplicates(arr);
+}

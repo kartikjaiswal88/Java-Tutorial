1042a2dc628abbd0870bad3055046ecc6ea43265
Kartik Jaiswal <kartikjaiswal038@gmail.com>
2024-02-06 12:49:55 +0530

    Jagged Array

--- ArraysConcept.java
+++ ArraysConcept.java
@@ -7,7 +7,6 @@ public class ArraysConcept {
         }
 
         int arr1[][] = new int[4][7];
-
         for (int i = 0; i < arr1.length; i++) {
             for (int j = 0; j < arr1[0].length; j++) {
                 arr1[i][j] = (int) (Math.random() * 100);
@@ -24,6 +23,22 @@ public class ArraysConcept {
         for (int n[] : arr1) {
             for (int m : n) {
                 System.out.print(m + " ");
+            }
+            System.out.println();
+        }
+
+        int num[][] = new int[3][]; // Jagged Array
+        num[0] = new int[5];
+        num[1] = new int[6];
+        num[2] = new int[3];
+        for (int i = 0; i < num.length; i++) {
+            for (int j = 0; j < num[i].length; j++) {
+                num[i][j] = (int) (Math.random() * 100);
+            }
+        }
+        for (int[] m : num) {
+            for (int n : m) {
+                System.out.print(n + " ");
             }
             System.out.println();
         }

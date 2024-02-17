bc61a6ce4bcb6d5362af4fcafe719be42b35a0e0
Kartik Jaiswal <kartikjaiswal038@gmail.com>
2024-02-17 05:47:54 +0530

    Super()AndThis()Concept

---
+++ NamingConvention.java
@@ -0,0 +1,12 @@
+public class NamingConvention {
+    public static void main(String[] args) {
+        
+        /*
+          class and Interfaces: Demo, Hello
+          variables and methods: showDetails(), getName()
+          constants: PIE, BRAND 
+
+
+         */
+    }
+}
---
+++ SuperConcept.java
@@ -0,0 +1,30 @@
+// Every constructor has a method call: super()
+class A extends Object {
+    public A() {
+        super();
+        System.out.println("In A");
+    }
+
+    public A(int n) {
+        super();
+        System.out.println("In A int");
+    }
+}
+
+class B extends A {
+    public B() {
+        super();
+        System.out.println("In B");
+    }
+
+    public B(int n) {
+        this();  //Executing the same class constructor
+        System.out.println("In B int");
+    }
+}
+
+public class SuperConcept {
+    public static void main(String[] args) {
+        B obj = new B(5);
+    }
+}
